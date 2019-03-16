43f5ab12db9001f3398814b6268d64819327fdb1
CorentinRenard <corentin.renard@epitech.eu>
2019-03-16 17:50:29 +0100

    Fix error stack digit in array

--- src/ui/ui_display_nbr.c
+++ src/ui/ui_display_nbr.c
@@ -9,6 +9,8 @@
 
 void refresh_display_nbr_array(size_t new_value)
 {
+    for (int i = 0; i < _ui.display_nbr_array.nb_digit; i++)
+        _ui.display_nbr_array.digits[i].value = 0;
     for (int i = 0; new_value > 0; i++) {
         _ui.display_nbr_array.digits
         [(_ui.display_nbr_array.nb_digit - 1) - i].value = new_value % 10;
