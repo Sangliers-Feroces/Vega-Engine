5a97fc7e15d5857deb1eecd5e62468a00d0f8893
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-03-17 15:37:44 +0100

    Final last commit about that bug

--- src/editor/editor_grab_replace.c
+++ src/editor/editor_grab_replace.c
@@ -7,22 +7,24 @@
 
 #include "headers.h"
 
-static void octree_replace_triangles(demo_t *demo, octree *tree)
+static int octree_replace_triangles(demo_t *demo, octree *tree)
 {
     if (tree == NULL)
-        return;
+        return 0;
     for (size_t i = 0; i < 8; i++)
-        octree_replace_triangles(demo, tree->sub[i]);
+        if (octree_replace_triangles(demo, tree->sub[i]))
+            return 1;
     for (size_t i = 0; i < tree->triangles.count; i++)
         if (!is_triangle_in_bounds(tree->triangles.triangle[i]->vertex,
         tree->bounds)) {
             rtx_triangle_replace(demo, tree, tree->triangles.triangle[i]);
             i--;
-            return;
+            return 1;
         }
+    return 0;
 }
 
 void editor_grab_replace(demo_t *demo)
 {
-    octree_replace_triangles(demo, demo->tree);
+    while (octree_replace_triangles(demo, demo->tree));
 }
