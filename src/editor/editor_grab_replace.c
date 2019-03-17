c7def332fbc3a7bc5d42984f498ad6d744c39620
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-03-17 15:25:00 +0100

    Final fix

--- src/editor/editor_grab_replace.c
+++ src/editor/editor_grab_replace.c
@@ -7,22 +7,6 @@
 
 #include "headers.h"
 
-static int is_point_selected(demo_t *demo, vec3 p)
-{
-    for (size_t i = 0; i < demo->editor.selections.count; i++)
-        if (vec3_eq(demo->editor.selections.selection[i], p))
-            return 1;
-    return 0;
-}
-
-static int is_triangle_selected(demo_t *demo, rtx_triangle *triangle)
-{
-    for (size_t i = 0; i < 3; i++)
-        if (is_point_selected(demo, triangle->vertex[i])) 
-            return 1;
-    return 0;
-}
-
 static void octree_replace_triangles(demo_t *demo, octree *tree)
 {
     if (tree == NULL)
@@ -30,9 +14,11 @@ static void octree_replace_triangles(demo_t *demo, octree *tree)
     for (size_t i = 0; i < 8; i++)
         octree_replace_triangles(demo, tree->sub[i]);
     for (size_t i = 0; i < tree->triangles.count; i++)
-        if (is_triangle_selected(demo, tree->triangles.triangle[i])) {
+        if (!is_triangle_in_bounds(tree->triangles.triangle[i]->vertex,
+        tree->bounds)) {
             rtx_triangle_replace(demo, tree, tree->triangles.triangle[i]);
             i--;
+            return;
         }
 }
 
