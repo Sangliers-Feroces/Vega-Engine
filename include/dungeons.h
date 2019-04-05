b7ab59cf7a5f7cf78e4b778bbb15a5e4fd03dbbf
CorentinRenard <corentin.renard@epitech.eu>
2019-04-05 09:28:36 +0200

    Add dungeons.h

---
+++ include/dungeons.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** MUL_my_world_2018
+** File description:
+** dungeons
+*/
+
+#ifndef DUNGEONS_H_
+    #define DUNGEONS_H_
+
+#define DUGEONS_ALLOC   3
+void dungeons_gen_init(demo_t *demo);
+
+void dungeons_render(demo_t *demo);
+vec_rtx_triangle *dungeons_gen_all(size_t rooms_limit);
+#endif /* !DUNGEONS_H_ */
