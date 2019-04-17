38c0399617b7518078b7205bf2acbf262ce69332
CorentinRenard <corentin.renard@epitech.eu>
2019-04-17 12:03:30 +0200

    Add setting effect

--- src/menu/setting_effect.c
+++ src/menu/setting_effect.c
@@ -7,21 +7,64 @@
 
 #include "headers.h"
 
+void display_x(demo_t *demo)
+{
+    vec2 screen = {demo->win.w , demo->win.h};
+    vec2 mouse_posf = {demo->mouse.mouse_pos.x, demo->mouse.mouse_pos.y};
+    vec2 relative_mouse_pos;
+
+    relative_mouse_pos = vec2_muls(
+    vec2_subs(vec2_div(mouse_posf, screen), 0.5f), 2.0f);
+    relative_mouse_pos.y *= -1.0;
+    printf("%f\n", relative_mouse_pos.x);
+}
+
+void move_slider(demo_t *demo, entity2_t *slider)
+{
+    vec2 screen = {demo->win.w , demo->win.h};
+    vec2 mouse_posf = {demo->mouse.mouse_pos.x, demo->mouse.mouse_pos.y};
+    vec2 relative_mouse_pos;
+
+    relative_mouse_pos = vec2_muls(
+    vec2_subs(vec2_div(mouse_posf, screen), 0.5f), 2.0f);
+    relative_mouse_pos.y *= -1.0;
+    //printf("%f\n", (relative_mouse_pos.x - slider->rect.p.x));
+    slider->rect.p.x += (relative_mouse_pos.x - slider->rect.p.x);
+}
+
+int check_slider_side(entity2_t slider)
+{
+    if (slider.slider->rect.p.x <= slider.x_min)
+        return 1;
+    return 0;
+}
+
 void setting_buttons_effects(demo_t *demo, menu_t *menu, setting_entity_t choice)
 {
+    float save_x;
+    display_x(demo);
     switch (choice) {
         case SETTING_RES_MINUS:
-            if (menu->setting[SETTING_RES_CHOICE].index > IUTEX_SETTING_RES_1600_900) {
-                menu->setting[SETTING_RES_CHOICE].index--;
+            if (menu->choosen_res > LIST_RES_1600_900) {
+                menu->choosen_res--;
                 menu->edited_setting = 1;
             }
             return;
         case SETTING_RES_PLUS:
-            if (menu->setting[SETTING_RES_CHOICE].index < IUTEX_SETTING_RES_1920_1080) {
-                menu->setting[SETTING_RES_CHOICE].index++;
+            if (menu->choosen_res < LIST_RES_1920_1080) {
+                menu->choosen_res++;
                 menu->edited_setting = 1;
             }
             return;
+        case SETTING_VOL_MASTER:
+            menu->current_slider = SETTING_VOL_MASTER;
+            if (!demo->mouse.button_release) {
+                save_x = menu->setting[SETTING_VOL_MASTER].rect.p.x;
+                move_slider(demo, menu->setting[SETTING_VOL_MASTER].slider);
+                if (check_slider_side(menu->setting[SETTING_VOL_MASTER]))
+                    menu->setting[SETTING_VOL_MASTER].slider->rect.p.x = save_x;
+            }
+            break;
         default:
             return;
     }
