859b17749c9afe4cf5f633abdb40530e2e462c3d
maxime <maxime.charron@epitech.eu>
2019-04-24 14:17:18 +0200

    Add model loader version 1.0

--- src/model/model_load.c
+++ src/model/model_load.c
@@ -7,3 +7,29 @@
 
 #include "headers.h"
 
+mesh_full_t *load_obj(const char *filepath)
+{
+    mesh_full_t *obj = mesh_full_create(-1, 0);
+    char line[128];
+    FILE *file = fopen(filepath, "rb");
+    int res;
+    vec3 pos;
+    vec2 uv;
+    vec3 normal;
+    vertex_t vert;
+
+    while (1) {
+        res = fscanf(file, "%s", line);
+        if (res == EOF)
+            break;
+        if (strcmp(line, "v") == 0)
+            fscanf(file, "%f %f %f", pos.x, pos.y, pos.z);
+        else if (strcmp(line, "vt" ) == 0)
+            fscanf(file, "%f %f", uv.x, uv.y );
+        else if (strcmp(line, "vn" ) == 0)
+            fscanf(file, "%f %f %f", normal.x, normal.y, normal.z);
+        vert = vertex_init(pos, normal, uv);
+        obj = 
+    }
+    return (obj);
+}
