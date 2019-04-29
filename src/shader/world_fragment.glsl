cb4d00ce7c4d324e4cfcecf0ca3083441fbc6c8d
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-04-29 20:31:56 +0200

    Adjust colors a bit

--- src/shader/world_fragment.glsl
+++ src/shader/world_fragment.glsl
@@ -15,11 +15,12 @@ uniform vec3 p_cam;
 
 void main(void)
 {
-	vec3 blue = (vec3(94.0, 155.0, 255.0) / 255.0) * 0.001;
+    vec3 blue = vec3(40, 90, 256) / 255;
 	vec3 light;
     vec3 v = normalize(p_cam - pos);
+    vec3 yellow = vec3(1.0, 1.0, 0.15);
 
-    light = vec3(max(0.0, dot(normal, l_dir)));
+    light = vec3(max(0.0, dot(normal, l_dir))) * yellow + blue * 0.2;
     float ff = pos.y < -42.0 ? (1.0 - min(1.0, (-pos.y - 42.0) / 40.0)) * 0.6 + 0.1 : 1.0;
 	color = vec4(light * texture(tex_albedo, uv_albedo).xyz * vec3(ff, ff, 1.0), 1.0);
     dist = vec4(vec3(length(p_cam - pos)), 1.0);
