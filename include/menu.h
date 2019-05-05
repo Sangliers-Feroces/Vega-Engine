eaf92ae39dc2013b1a5450793629c3a629b81ac9
CorentinRenard <corentin.renard@epitech.eu>
2019-05-05 21:13:00 +0200

    And his name is MENU.H

--- include/menu.h
+++ include/menu.h
@@ -21,3 +21,7 @@ void menu_display_help(void);
 
 int menu_poll_events(demo_t *demo, menu_t *menu);
 float menu_slider_move(demo_t *demo, entity2_t *slider);
+
+int check_music_match_time(sfTime expected, musics_t index);
+size_t music_play_mod(size_t mod, musics_t index);
+void music_check_breakpoint(int *display_vm);
