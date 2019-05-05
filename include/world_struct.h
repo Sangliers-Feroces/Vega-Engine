23583dc119b3d2e0d9aa8189e58034df6293db08
Adrien LENGLET <adrien.lenglet3@gmail.com>
2019-05-05 12:27:22 +0200

    Oops

--- include/world_struct.h
+++ include/world_struct.h
@@ -14,7 +14,7 @@
 #define CHUNK_TERRAIN_SUB_SIZE ((ssize_t)(CHUNK_SIZE_TERRAIN / CHUNK_SIZE))
 #define CHUNK_GEN_ITER 6
 #define CHUNK_GEN_EXT_ITER 2
-#define CHUNK_LOAD_DISTANCE 2
+#define CHUNK_LOAD_DISTANCE 8
 
 typedef struct {
     size_t lod;
